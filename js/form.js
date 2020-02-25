61fec7ba5a9ce5a6030a23297a7fd6174616f65c
Patryk <patrykyrtap@o2.pl>
2020-02-25 20:39:43 +0100

    Add form, section courses
    Modify image hover, start using formspree

---
+++ js/form.js
@@ -0,0 +1,21 @@
+$('#contact-form').submit(function(e){
+    const name = document.getElementById('name'),
+          email = document.getElementById('email'),
+          content = document.getElementById('content');
+})
+
+if(!name.value || !email.value || !content.value){
+    alertify.error('Wszystkie pola muszą być uzupełnione!')
+} else {
+
+    $.ajax({
+        url: "https://formspree.io/patrykyrtap@o2.pl",
+        method: "POST",
+        data: $(this).serialize(),
+        dataType: "json"
+    });
+
+    e.preventDefault();
+    $(this).get(0).reset();
+    alertify.success('Wiadomość została wysłana!');
+}
