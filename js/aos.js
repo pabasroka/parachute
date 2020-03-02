9e34469110f76b5cd438869657ad410a3fc5b246
Patryk <patrykyrtap@o2.pl>
2020-03-02 21:49:05 +0100

    Make website responsive for every device

--- js/aos.js
+++ js/aos.js
@@ -1,5 +1,8 @@
 AOS.init({
-    offset: 300, // offset (in px) from the original trigger point
+    offset: 150, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 600 // values from 0 to 3000, with step 50ms
+    duration: 600, // values from 0 to 3000, with step 50ms
+    disable: function () {
+        return window.innerWidth > 960;
+    }
 });
